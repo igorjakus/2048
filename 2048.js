77e66ff2aa4c35bedca64ad132be66f25dcccd42
igorjakus <igorjakus@protonmail.com>
2022-07-25 20:48:28 +0200

    improve game looks & refactoring

--- 2048.js
+++ 2048.js
@@ -1,19 +1,13 @@
 var canvas = document.getElementById("2048-game");
 var ctx = canvas.getContext("2d");
 
-// background
-ctx.fillStyle = "#ff9980";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-var padding = 15;
-var rectx = (600 / 4) - 2*padding;
-var recty = (600 / 4) - 2*padding;
-
-for (let x = 0; x < 4; x++) {
-    for (let y = 0; y < 4; y++) {
-        ctx.fillStyle = "gray";
-        ctx.fillRect((x*150)+padding, (y*150)+padding, rectx, recty);
-    }
+function initializeArray() {
+    var grid = [[0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0]];
+    return grid;
 }
 
 function newNumber() {
@@ -28,37 +22,64 @@ function newNumber() {
     if (Math.round(Math.random()*10) == 0) {
         value = 4;
     }
+
     array[i][j] = value;
 }   
 
-function initializeArray() {
-    var grid = [[0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0]];
-    return grid;
+function drawBackground() {
+    ctx.fillStyle = "silver";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-var array = initializeArray();
-newNumber();
-newNumber();
-console.log(array)
-drawNumbers();
+function drawSquares() {
+    const padding = 20;
+    const rect = (600 - 5*padding) / 4;
+    const space = rect + padding;
 
-function drawNumbers() {
-    ctx.font = "50px Arial";
     ctx.textAlign = "center";
-    ctx.fillStyle = "black";
+    ctx.font = "50px Arial";
+    const charHeight = 36
+
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
-            x = array[i][j]
-            if(x != 0) {
-                // koordynaty lewego dolnego rogu jezeli nie align center
-                // koordynaty srodka dolu jezeli align center
-                // 36px = wysokosc znaku dla font 50px
-                // 75px = polowa kwadratu 150px
-                ctx.fillText(x, j*150 + 75, i*150 + 36/2 + 75)
+            value = array[i][j];
+            posX = j*space + padding;
+            posY = i*space + padding;
+            ctx.fillStyle = color(value)
+            ctx.fillRect(posX, posY, rect, rect)
+
+            if(value != 0) {
+                ctx.fillStyle = "black";
+                ctx.fillText(value, posX + rect/2, posY + rect/2 + charHeight/2);
             }
         }        
     }
 }
+
+function color(x) {
+    if (x == 0) {
+        return "gainsboro"; 
+    }
+
+    n = Math.log2(x);
+    switch (n) {
+        case 1:
+            return "lightslategray";
+        case 2:
+            return "cadetblue";
+        case 3:
+            return "salmon";
+        case 4:
+            return "indigo";
+        default:
+            return "darkslategray";
+    }
+}
+
+
+var array = initializeArray();
+newNumber();
+newNumber();
+drawBackground();
+drawSquares();
+console.log(array);
