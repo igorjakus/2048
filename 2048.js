70a00ced390a556b2e9698ce3bf64ee8d96349e8
igorjakus <igorjakus@protonmail.com>
2022-07-26 12:24:09 +0200

    refactoring
    - move code to class Game
    - for 2^5 "crimson" color

--- 2048.js
+++ 2048.js
@@ -1,85 +1,88 @@
-var canvas = document.getElementById("2048-game");
-var ctx = canvas.getContext("2d");
+class Game {
+    constructor() {
+        this.canvas = document.getElementById("2048-game");
+        this.ctx = this.canvas.getContext("2d");
+        this.grid = [[0,0,0,0],
+                     [0,0,0,0],
+                     [0,0,0,0],
+                     [0,0,0,0]];
 
-
-function initializeArray() {
-    var grid = [[0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0]];
-    return grid;
-}
-
-function newNumber() {
-    do {
-        var x = Math.floor(Math.random() * 16); // random integer from 0 to 15 
-        var i = Math.floor(x/4);
-        var j = x % 4;
-        console.log(x, i, j)
-    } while (array[i][j] != 0);
-
-    var value = 2;
-    if (Math.round(Math.random()*10) == 0) {
-        value = 4;
+        this.addNumber();
+        this.addNumber();
     }
 
-    array[i][j] = value;
-}   
+    addNumber() {
+        // random position of new square
+        do {
+            var x = Math.floor(Math.random() * 16);
+            var i = Math.floor(x/4);
+            var j = x % 4;
+        } while (this.grid[i][j] != 0) // infinite loop when space
+        
+        // 90% of 2, 10% of 4
+        var value = 2;
+        if (Math.floor(Math.random()*10) == 0) {
+            value = 4;
+        }
 
-function drawBackground() {
-    ctx.fillStyle = "silver";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
+        // add new number
+        this.grid[i][j] = value
+    }
 
-function drawSquares() {
-    const padding = 20;
-    const rect = (600 - 5*padding) / 4;
-    const space = rect + padding;
+    drawBackground() {
+        this.ctx.fillStyle = "silver";
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
 
-    ctx.textAlign = "center";
-    ctx.font = "50px Arial";
-    const charHeight = 36
+    drawSquares() {
+        const padding = 20;
+        const rect = (600 - 5*padding) / 4;
+        const space = rect + padding;
 
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            value = array[i][j];
-            posX = j*space + padding;
-            posY = i*space + padding;
-            ctx.fillStyle = color(value)
-            ctx.fillRect(posX, posY, rect, rect)
+        this.ctx.textAlign = "center";
+        this.ctx.font = "50px Arial";
+        const charHeight = 36
 
-            if(value != 0) {
-                ctx.fillStyle = "black";
-                ctx.fillText(value, posX + rect/2, posY + rect/2 + charHeight/2);
-            }
-        }        
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                let value = this.grid[i][j];
+                let posX = j*space + padding;
+                let posY = i*space + padding;
+                this.ctx.fillStyle = this.color(value);
+                this.ctx.fillRect(posX, posY, rect, rect);
+
+                // if value == 0 then don't write numbers
+                if(value != 0) {
+                    this.ctx.fillStyle = "black";
+                    this.ctx.fillText(value, posX + rect/2, posY + rect/2 + charHeight/2);
+                }
+            }        
+        }
+    }
+
+    color(x) {
+        if (x == 0) {
+            return "gainsboro"; 
+        }
+    
+        let n = Math.log2(x);
+        switch (n) {
+            case 1:
+                return "lightslategray";
+            case 2:
+                return "cadetblue";
+            case 3:
+                return "salmon";
+            case 4:
+                return "indigo";
+            case 5:
+                return "crimson";
+            default:
+                return "darkslategray";
+        }
     }
 }
 
-function color(x) {
-    if (x == 0) {
-        return "gainsboro"; 
-    }
-
-    n = Math.log2(x);
-    switch (n) {
-        case 1:
-            return "lightslategray";
-        case 2:
-            return "cadetblue";
-        case 3:
-            return "salmon";
-        case 4:
-            return "indigo";
-        default:
-            return "darkslategray";
-    }
-}
-
-
-var array = initializeArray();
-newNumber();
-newNumber();
-drawBackground();
-drawSquares();
-console.log(array);
+game = new Game();
+game.drawBackground();
+game.drawSquares();
